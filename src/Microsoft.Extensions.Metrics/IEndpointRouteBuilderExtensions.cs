427d91b1eb4740c97c8aec8aa0c1485c5607dbf5
Sourabh Shirhatti <soshir@microsoft.com>
2019-06-10 17:53:09 -0700

    Initial commit

---
+++ src/Microsoft.Extensions.Metrics/IEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class IEndpointRouteBuilderExtensions
+    {
+        public static IEndpointRouteBuilder MapMetricsEndpoint(this IEndpointRouteBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder;
+        }
+    }
+}
